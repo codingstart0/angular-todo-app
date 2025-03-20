dc041a483b09224f0c369d43f7339b3fb5be4963
codingstart0 <129733643+codingstart0@users.noreply.github.com>
2025-03-20 09:41:41 +0200

    Set up (#1)
    * Update package.json
    
    * Update package-lock.json
    
    * Delete useless template from html files
    
    * Generate component for TODO app
    
    * Generate service
    
    * Create Todo Model using a class
    
    * Create Todo Model using a class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListComponent,
+    TodoItemComponent
   ],
   imports: [
     BrowserModule,
