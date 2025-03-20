d52dd92ff49e6f53db6cc6ba378529d44747824a
codingstart0 <codingstart0@gmail.com>
2025-03-20 12:56:13 +0200

    Use mat-checkbox in the template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCheckboxModule } from '@angular/material/checkbox';
@@ -13,6 +14,7 @@ import { TodoItemComponent } from './components/todo-item/todo-item.component';
   declarations: [AppComponent, TodoListComponent, TodoItemComponent],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
