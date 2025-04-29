df2cefcf1f4f292bc8c52639eaaad998726961f8
codingstart0 <129733643+codingstart0@users.noreply.github.com>
2025-04-29 14:12:00 +0300

    Vercel (#8)
    * Replace apiBaseUrl to fetch data from publicly accessible API endpoint
    
    * Replace PATCH method to PUT
    
    * Remove Partial  from function updateTodo
    
    * Move API base URL from TodoService to environment files

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Todo } from '../interfaces/todo.interface';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TodoService {
-  private apiBaseUrl = 'http://localhost:3000';
+  private apiBaseUrl = environment.apiBaseUrl;
 
   constructor(private http: HttpClient) {}
 
@@ -19,11 +20,8 @@ export class TodoService {
     return this.http.post<Todo>(`${this.apiBaseUrl}/todos`, todo);
   }
 
-  updateTodo(partialTodo: Partial<Todo> & { id: number }): Observable<Todo> {
-    return this.http.patch<Todo>(
-      `${this.apiBaseUrl}/todos/${partialTodo.id}`,
-      partialTodo
-    );
+  updateTodo(todo: Todo): Observable<Todo> {
+    return this.http.put<Todo>(`${this.apiBaseUrl}/todos/${todo.id}`, todo);
   }
 
   removeTodo(id: number): Observable<void> {
---
+++ src/environments/environment.production.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiBaseUrl: 'https://680209ec81c7e9fbcc44224e.mockapi.io',
+};
