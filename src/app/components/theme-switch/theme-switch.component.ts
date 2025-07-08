d9ad0003dffe4680647d4df6b05f3ee6ec5260c8
codingstart0 <codingstart0@gmail.com>
2025-07-08 15:24:58 +0300

    Revert "refactor(theme-switch): move theme toggle UI and logic to ThemeSwitchComponent"
    This reverts commit c6c95e145ffbe0399373a273469a2d32d5414b1a.

--- src/app/components/theme-switch/theme-switch.component.ts
+++ src/app/components/theme-switch/theme-switch.component.ts
@@ -1,19 +1,10 @@
 import { Component } from '@angular/core';
-import { ThemeService } from 'src/app/services/theme.service';
 
 @Component({
   selector: 'app-theme-switch',
   templateUrl: './theme-switch.component.html',
-  styleUrls: ['./theme-switch.component.scss'],
+  styleUrls: ['./theme-switch.component.scss']
 })
 export class ThemeSwitchComponent {
-  constructor(private themeService: ThemeService) {}
 
-  onToggleThemeClick(): void {
-    this.themeService.toggleTheme();
-  }
-
-  get isDarkTheme(): boolean {
-    return this.themeService.isDarkTheme();
-  }
 }
